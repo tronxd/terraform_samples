6c4c6aef3ba1bf1e04ad8e94e88e3c47bc70b100
tronxd <tron47@gmail.com>
2022-01-05 11:43:02 +0200

    tag resources

--- aws/s3.tf
+++ aws/s3.tf
@@ -6,6 +6,16 @@ resource "aws_s3_bucket" "sample_bucket_1" {
     enabled = true
   }
   force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "c0d78c48-5f9e-4187-a04e-a6bb0e7ed9f8"
+  }
 }
 
 resource "aws_s3_bucket" "sample_bucket_2" {
@@ -16,6 +26,16 @@ resource "aws_s3_bucket" "sample_bucket_2" {
     enabled = true
   }
   force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "3f1c4edc-992e-429a-8300-f6b48b795c66"
+  }
 }
 
 resource "aws_s3_bucket" "sample_bucket_3" {
@@ -26,6 +46,16 @@ resource "aws_s3_bucket" "sample_bucket_3" {
     enabled = true
   }
   force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "20295b9d-6fb8-4280-9a57-b2b9c22afe63"
+  }
 }
 
 resource "aws_s3_bucket" "sample_bucket_4" {
@@ -36,6 +66,16 @@ resource "aws_s3_bucket" "sample_bucket_4" {
     enabled = true
   }
   force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "00398d29-90e1-406f-b273-2525d31c26b2"
+  }
 }
 
 resource "aws_s3_bucket" "sample_bucket_5" {
@@ -46,4 +86,14 @@ resource "aws_s3_bucket" "sample_bucket_5" {
     enabled = true
   }
   force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "474aa4af-b536-4c80-9d2a-ae18d3479d04"
+  }
 }
